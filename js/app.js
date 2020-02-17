2747a1edb0b4149a4bddbefa9781b320721346e7
waldemarciupa <waldemarciupa90@gmail.com>
2020-02-17 10:08:13 +0100

    added black styling and new buttons

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const searchInput = document.querySelector(".gallery__input");
-const searchBtn = document.querySelector(".gallery__button");
+const searchBtn = document.querySelector(".gallery__button--search");
+const gallery = document.querySelector(".gallery");
 const galleryContainer = document.querySelector(".gallery__container");
-const loadBtn = document.querySelector(".load");
+const loadBtn = document.querySelector(".gallery__button--load");
 
 searchBtn.addEventListener("click", function(e) {
   e.preventDefault();
@@ -13,44 +14,55 @@ searchBtn.addEventListener("click", function(e) {
       per_page: 5
     }
   }).then(res => {
-    while (galleryContainer.firstChild)
-      galleryContainer.removeChild(galleryContainer.firstChild);
-    searchInput.value = "";
+    console.log(res.data.collection.items);
+    if (res.data.collection.items.length > 0) {
+      while (galleryContainer.firstChild)
+        galleryContainer.removeChild(galleryContainer.firstChild);
+      searchInput.value = "";
 
-    const eachItem = res.data.collection.items;
+      const eachItem = res.data.collection.items;
 
-    for (let i = 0; i < 16; i++) {
-      const imgUrl = eachItem[i].links[0].href;
-      const imageEl = document.createElement("img");
-      imageEl.src = imgUrl;
-      imageEl.classList.add("image");
-      galleryContainer.appendChild(imageEl);
-    }
-    let counter;
-    var start = 0;
-    var all = 16;
-    loadBtn.addEventListener("click", function() {
-      counter++;
-      if (counter === 0) {
-        start = start;
-        all = all;
-      } else {
-        start = start + 16;
-        all = all + 16;
-      }
-
-      for (var i = start; i < all; i++) {
+      for (let i = 0; i < 15; i++) {
         const imgUrl = eachItem[i].links[0].href;
         const imageEl = document.createElement("img");
         imageEl.src = imgUrl;
         imageEl.classList.add("image");
         galleryContainer.appendChild(imageEl);
       }
-    });
-    // for (const el of eachItem) {
-    //   const imageEl = document.createElement("div");
-    //   imageEl.innerHTML = `<img class="image" src="${el.links[0].href}">`;
-    //   galleryContainer.appendChild(imageEl);
-    // }
+      let counter;
+      var start = 0;
+      var all = 15;
+      loadBtn.addEventListener("click", function() {
+        counter++;
+        if (counter === 0) {
+          start = start;
+          all = all;
+        } else {
+          start = start + 15;
+          all = all + 15;
+        }
+
+        for (var i = start; i < all; i++) {
+          const imgUrl = eachItem[i].links[0].href;
+          const imageEl = document.createElement("img");
+          imageEl.src = imgUrl;
+          imageEl.classList.add("image");
+          galleryContainer.appendChild(imageEl);
+        }
+      });
+      // for (const el of eachItem) {
+      //   const imageEl = document.createElement("div");
+      //   imageEl.innerHTML = `<img class="image" src="${el.links[0].href}">`;
+      //   galleryContainer.appendChild(imageEl);
+      // }
+    } else {
+      while (galleryContainer.firstChild)
+        galleryContainer.removeChild(galleryContainer.firstChild);
+      searchInput.value = "";
+      const errorBox = document.createElement("div");
+      errorBox.innerHTML = `<p style="color: white">Oops, something went wrong! Probably there is no data like you are looking for...</p>`;
+
+      gallery.appendChild(errorBox);
+    }
   });
 });
