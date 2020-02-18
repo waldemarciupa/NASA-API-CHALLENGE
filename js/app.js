83cdf0e74ed2b59466af0e5ca9855f55f1a63ff3
waldemarciupa <waldemarciupa90@gmail.com>
2020-02-18 18:47:11 +0100

    working on modal

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-const galleryForm = document.querySelector(".gallery__form");
-const searchInput = document.querySelector(".gallery__input");
-const searchBtn = document.querySelector(".gallery__button--search");
-const gallery = document.querySelector(".gallery");
-const galleryContainer = document.querySelector(".gallery__container");
-const loadBtn = document.querySelector(".gallery__button--load");
+const galleryForm = document.querySelector('.gallery__form');
+const searchInput = document.querySelector('.gallery__input');
+const searchBtn = document.querySelector('.gallery__button--search');
+// const gallery = document.querySelector(".gallery");
+const galleryContainer = document.querySelector('.gallery__container');
+const loadBtn = document.querySelector('.gallery__button--load');
 
 
 
-searchBtn.addEventListener("click", function (e) {
+searchBtn.addEventListener('click', function (e) {
   e.preventDefault();
   const q = searchInput.value;
   axios({
-    method: "get",
+    method: 'get',
     url: `https://images-api.nasa.gov/search?q=${q}`,
     data: {
       per_page: 5
@@ -24,75 +24,169 @@ searchBtn.addEventListener("click", function (e) {
         galleryContainer.removeChild(galleryContainer.firstChild);
       searchInput.value = "";
 
+      // load gallery
+
       const eachItem = res.data.collection.items;
-
-      // Function loadGallery 
-      // function loadGallery(i) {
-      //   const imgUrl = eachItem[i].links[0].href;
-      //   const imageEl = document.createElement("img");
-      //   imageEl.src = imgUrl;
-      //   imageEl.classList.add("image");
-      //   galleryContainer.appendChild(imageEl);
-
-      // }
 
       function loadGallery(i) {
         const imgUrl = eachItem[i].links[0].href;
-        const imageEl = document.createElement("div");
+        const imageTitle = eachItem[i].data[0].title;
+        const imageDescription = eachItem[i].data[0].description;
+        const imageEl = document.createElement('div');
+
+        // create image
         imageEl.classList.add('imageBox');
         imageEl.innerHTML =
-          `<img class="image" src="${imgUrl}">`
-
-        // imgUrl;
-        // imageEl.classList.add("image");
+          `<img class="image" 
+            src="${imgUrl}"
+            title="${imageTitle}"
+            data-description="${imageDescription}"
+            >`
         galleryContainer.appendChild(imageEl);
 
+        // checking is it end of data
+        const galleryImages = document.querySelectorAll('.gallery__container div');
+        if (res.data.collection.items.length === galleryImages.length) {
+          loadBtn.classList.remove('show');
+          loadBtn.classList.add('hide');
+          const infoBox = document.createElement('div');
+          infoBox.innerHTML = `<p style="color: white">There is no more data.</p>`;
+          galleryContainer.appendChild(infoBox);
+        }
+
+        // gallery
+        function gallery(gallery) {
+          if (!gallery) {
+            throw new Error('No gallery found')
+          }
+          const images = Array.from(galleryImages);
+          // console.log(images);
+          const modal = document.querySelector('.gallery__modal');
+          const prevBtn = document.querySelector('.prev')
+          const nextBtn = document.querySelector('.next')
+          let currentImage;
+
+          function openModal() {
+            // checking is modal open
+            if (modal.matches('.open')) {
+              console.info('Modal already open');
+              return;
+            }
+            modal.classList.add('open');
+
+            window.addEventListener('keyup', keyUpEscape);
+            nextBtn.addEventListener('click', showNextImage);
+          }
+
+          function showNextImage(e) {
+            console.log(e);
+            e.stopImmediatePropagation();
+
+            console.log(currentImage.nextElementSibling);
+
+          }
+
+          function closeModal() {
+            modal.classList.remove('open');
+
+            window.removeEventListener('keyup', keyUpEscape);
+            nextBtn.removeEventListener('click', showNextImage);
+          }
+
+          function clickOutside(e) {
+            e.stopImmediatePropagation();
+            if (e.target === e.currentTarget) {
+              closeModal();
+            }
+          }
+
+          function keyUpEscape(e) {
+            e.stopImmediatePropagation();
+            if (e.key === 'Escape') closeModal();
+          }
+
+          function showImage(el) {
+            if (!el) {
+              console.log('there is no image to show!');
+              return
+            } else {
+              console.dir(el);
+
+              modal.querySelector('img').src = el.firstChild.src;
+              modal.querySelector('h2').textContent = el.firstChild.title;
+              modal.querySelector('p').textContent = el.firstChild.dataset.description;
+              currentImage = el;
+              openModal();
+            }
+          }
+
+          // events
+
+          images.forEach(function (image) {
+            image.addEventListener('click', function (event) {
+              event.stopImmediatePropagation();
+              showImage(event.currentTarget);
+            })
+          })
+
+          modal.addEventListener('click', clickOutside);
+
+
+        }
+
+
+        gallery(galleryContainer)
       }
 
       // load first gallery
       for (let i = 0; i < 20; i++) {
-        loadGallery(i)
+        if (eachItem[i].links) {
+          loadGallery(i)
+        } else {
+          const errorBox = document.createElement('div');
+          errorBox.innerHTML = `<p style="color: white">Oops, something went wrong!</p>`;
+          galleryContainer.appendChild(errorBox);
+          return
+        }
       }
 
-      loadBtn.classList.remove("hide");
-      loadBtn.classList.add("show");
+      loadBtn.classList.remove('hide');
+      loadBtn.classList.add('show');
 
       // load more
-      let counter;
-      let start = 0;
-      let all = 20;
+      // let counter;
+      // let start = 0;
+      // let all = 20;
 
-      loadBtn.addEventListener("click", function () {
-        counter++;
-        if (counter === 0) {
-          start = start;
-          all = all;
-        } else {
-          start = start + 20;
-          all = all + 20;
-        }
+      let counter = 20;
 
-        for (var i = start; i < all; i++) {
-          loadGallery(i)
+      loadBtn.addEventListener('click', function () {
+        // counter++;
+        // if (counter === 0) {
+        //   start = start;
+        //   all = all;
+        // } else {
+        //   start = start + 20;
+        //   all = all + 20;
+        // }
+
+        for (let i = counter; i < counter + 20; i++) {
+          if (eachItem[i]) {
+            loadGallery(i)
+          } else {
+            break;
+          }
         }
-        const galleryImages = document.querySelectorAll('.gallery__container img');
-        if (res.data.collection.items.length === galleryImages.length) {
-          console.log('its over');
-          loadBtn.classList.remove("show");
-          loadBtn.classList.add("hide");
-          const infoBox = document.createElement("div");
-          infoBox.innerHTML = `<p style="color: white">There is no more data.</p>`;
-          galleryContainer.appendChild(infoBox);
-        }
+        counter += 20;
       });
 
     } else {
       while (galleryContainer.firstChild)
         galleryContainer.removeChild(galleryContainer.firstChild);
       searchInput.value = "";
-      loadBtn.classList.remove("show");
-      loadBtn.classList.add("hide");
-      const errorBox = document.createElement("div");
+      loadBtn.classList.remove('show');
+      loadBtn.classList.add('hide');
+      const errorBox = document.createElement('div');
       errorBox.innerHTML = `<p style="color: white">Oops, something went wrong! Probably there is no data like you are looking for...</p>`;
       galleryContainer.appendChild(errorBox);
     }
